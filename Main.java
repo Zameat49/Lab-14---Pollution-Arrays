67a07189b6f1278fb6ec9f53cd8634d644a1bd01
LAB4804WD22 <jakezamir@gmail.com>
2026-07-09 11:34:36 -0700

    Creates a bar graph checking CO2 Levels.

--- Main.java
+++ Main.java
@@ -33,15 +33,24 @@ class Main
   public static void main(String[] args) 
   {
 		// DECLARATION SECTION
+    double[] co2Levels = {371.32, 373.45, 375.98, 377.70, 379.98, 382.09, 384.03, 385.83, 387.64, 390.10, 391.85, 394.06, 396.74, 398.87, 401.01, 404.41, 406.76, 408.72, 411.66, 414.24};
+
+    int[] years = new int[20];
+
+    for (int i = 0; i < years.length; i++)
+    {
+      years[i] = 2001 + i;
+    }
+
 
 		// INITIALIZATION SECTION
 
 		// INPUT SECTION 
 
 		// PROCESSING SECTION
-
+    double difference = co2Levels[co2Levels.length - 1]  - co2Levels[0];
 		// OUTPUT SECTION
-  
+    printGraph(co2Levels, years);
     //Explanation of oil drum units
     System.out.println("\nData displayed for each year begins at 360 ppm.\nEach additional oil drum (🛢 ) represents an additional 10 ppm.\n");
 
@@ -49,6 +58,26 @@ class Main
     System.out.print("Year");
     UtilityBelt.printCentered(55,"CO₂ in Atmosphere (ppm)");
 
+  }
+  public static void printBar(double co2Amount)
+  {
+    int numOfBarrels = (int) (co2Amount) - 360;
+
+    for (int i = 0; i < numOfBarrels; i++)
+    {
+      System.out.print("🛢");
+
+    }
+    System.out.printf(" %.2f%n", co2Amount);
+  }
+
+  public static void printGraph(double[] co2Levels, int[] years)
+  {
+    for (int i = 0; i < years.length; i++)
+    {
+      System.out.print(years[i] + " ");
+      printBar(co2Levels[i]);
+    }
   }
 
 }
